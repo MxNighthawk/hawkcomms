0a4ef4f36016349bf7d80135a75d09a23af170d1
MxNighthawk <mxnighthawk@gmail.com>
2024-12-01 01:18:55 -0800

    v0.19.0003

--- JS/navigation.js
+++ JS/navigation.js
@@ -11,7 +11,7 @@ let menu = nav.getElementsByTagName("button");
 let mini = document.getElementById("mobileMenuToggle");
 
 let selectedTool = document.getElementById(tools[0]);
-let showMenu = false, showMini = false;
+let showMenu = false;
 
 selectedTool.style.setProperty("display", "block");
 menu[0].classList.add("selectedTool");
@@ -21,13 +21,17 @@ window.onresize = () =>
 	if(window.innerWidth > 500)
 	{
 		nav.style.setProperty("display", "flex");
-		showMini = showMenu = false;
+		mini.style.setProperty("display", "none");
 	}
 	else
 	{
 		nav.style.setProperty("display", "none");
-		showMini = showMenu = true;
+
+		if(portfolioWarning.style.getPropertyValue("opacity") == 0)
+			mini.style.setProperty("display", "block");
 	}
+	
+	showMenu = false;
 };
 
 function SetToolVisiblitiy(id)
@@ -44,14 +48,13 @@ function SetToolVisiblitiy(id)
 
 function HideMiniMenu()
 {
-	if(needsAWarning)
-		mini.style.setProperty("display", "none");
-	
 	if(window.innerWidth > 500)
 		return;
 
-	mini.style.setProperty("display", showMini ? "block" : "none");
-	showMini = !showMini;
+	if(portfolioWarning.style.getPropertyValue("opacity") == 1)
+		mini.style.setProperty("display", "none");
+	else
+		mini.style.setProperty("display", "block");
 }
 
 function ShowMobileNav()
